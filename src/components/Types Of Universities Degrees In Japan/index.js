c697861b7732199f9248f05f4a38fed4386b1f4b
md-moniruzzaman01 <md.moniruzzaman1050@gmail.com>
2022-12-24 00:15:41 +0600

    with out info section of type of university degrees

--- src/components/Types Of Universities Degrees In Japan/index.js
+++ src/components/Types Of Universities Degrees In Japan/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Breadcrumbs from '../shared/Breadcrumbs';
 import HeaderOfPage from '../shared/HeaderOfPage';
 import slide1 from '../../image/slide1.2.jpg'
-import InfoPart from './components/info section';
+// import InfoPart from './components/info section';
 import DegreesSection from './components/degrees';
 const TypesOfUniversitiesDegreesInJapanPage = () => {
     return (
         <div>
             <HeaderOfPage img={slide1}><h1>TYPES OF UNIVERSITIES & DEGREES IN JAPAN</h1></HeaderOfPage>
            <Breadcrumbs pageName='Types Of Universities & Degrees In Japan'/>
-           <InfoPart/>
+           {/* <InfoPart/> */}
            <DegreesSection/>
         </div>
     );
